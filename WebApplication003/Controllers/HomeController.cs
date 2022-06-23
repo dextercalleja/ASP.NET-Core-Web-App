17ac70223f70ed90d23b1242bc2c64b33ca9d4ca
Dexter Calleja Jr <dexterjr.calleja@sykes.com>
2022-06-24 01:12:29 +0800

    Add project files.

---
+++ WebApplication003/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication003.Data;
+using WebApplication003.Models;
+
+namespace WebApplication003.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _dbContext;
+        public HomeController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            //routing
+            //localhost://54329/Home/Index
+            //localhost://54329
+            return View();
+        }
+
+        public IActionResult Post()
+        {
+            //localhost://54329/Home/Post
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Post());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Post post)
+        {
+            _dbContext.Posts.Add(post);
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(this.Post));
+        }
+    }
+}
---
+++ WebApplication003/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication003.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public Post()
+        {
+            Title = string.Empty;
+            Content = string.Empty;
+            Id = 0;
+        }
+    }
+}
